847ec5e06ee76ddafeaf33947a02c962edca799b
Isabelascolla <102077830+Isabelascolla@users.noreply.github.com>
2022-12-15 19:17:10 -0300

    Add files via upload

---
+++ import pandas as pd.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from classe_pokemon import pokemon
+
+def gerar_pokemon():
+    pokemons=pd.read_csv(time_1.csv)
+
+
+c=0
+while c<=5:
+
+    l=[]
+    for i in range(len(pokemons.columns))
